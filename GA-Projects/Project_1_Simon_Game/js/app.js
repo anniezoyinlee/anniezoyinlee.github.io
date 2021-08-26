dc0aeb3e05144fed1583ded906d31aa8f414f8d3
Annie Lee <annie80113@hotmail.com>
2021-08-26 12:37:35 -0400

    cleaned the code

--- GA-Projects/Project_1_Simon_Game/js/app.js
+++ GA-Projects/Project_1_Simon_Game/js/app.js
@@ -8,14 +8,14 @@ const sidebar = document.querySelector('.sidebar');
 const difficulity = document.querySelector('.difficulity');
 const title = document.querySelector('.title');
 const levelDisplay = document.querySelector('.levelDisplay');
-const status = document.querySelector('.status')
+const statusDisplay = document.querySelector('.statusDisplay')
 const hard = document.getElementById('hard');
 const easy = document.getElementById('easy');
 const start = document.getElementById('startBtn');
 const restart = document.getElementById('restartBtn');
 const changeDifficulity = document.getElementById('changeDifficulity');
 const colorblock = document.querySelector('.colorblock');
-const targetLevel = document.getElementById('targetLevel');
+const status = document.getElementById('status');
 const bestLevel = document.getElementById('bestLevel');
 const currentLevel = document.getElementById('currentlevel');
 
@@ -56,7 +56,7 @@ const createSequence = (level) => {
 
 // show sequence by making blocks change style(opacity and border), then change back
 const showSequence = () => {
-	colorblock.style.border = 'red 2px dashed';
+	colorblock.style.border = 'deeppink 3px dashed';
 	for (i = 0; i < sequence.length; i++) {
 		const block = sequence[i];
 		// blocks start changing one by one after the function is called
@@ -72,7 +72,7 @@ const showSequence = () => {
 		// make it user's turn after the last block change back
 		setTimeout(() => {
 				colorblock.classList.remove('unclickable');
-				colorblock.style.border = 'gold 2px dashed';
+				colorblock.style.border = 'gold 3px dashed';
 		}, 500 + (sequence.length * 2000));
 	}
 }
@@ -149,7 +149,7 @@ const playGame = (colorsAmount) => {
 	colorblock.style.display = 'flex';
 	title.style.fontSize = '1.5vw';
 	title.style.margin = '0 0 0 10%';
-	sidebar.insertBefore(title, status);
+	sidebar.insertBefore(title, statusDisplay);
 	levelDisplay.style.display = 'block';
 	createColors(colorsAmount);
 	playLevel();
@@ -159,7 +159,7 @@ const playLevel = () => {
 	colorblock.classList.add('unclickable');
 	sequence = [];
   // Shows the target level for player to win
-	targetLevel.innerText = 'Finish Level ' + target + ' to win!';
+	status.innerText = 'Finish Level ' + target + ' to win!';
 	// As a player, I would like to know what is the highest level I ever at
 	bestLevel.innerText = 'Best Record: Level ' + best;
 	// As a player, I would like to know which level I'm at
@@ -173,7 +173,7 @@ const goNextLevel = () => {
 	if (level < target) {
 		// Shows good word if player complete a level
 		const goodWords = ['Good Job! ', 'Fantastic! ', 'Wonderful! ', 'Awesome! ', 'Nice! ']
-		targetLevel.innerText = goodWords[Math.floor(Math.random() * (goodWords.length - 1))] + 'Go to level ' + (level + 1);
+		status.innerText = goodWords[Math.floor(Math.random() * (goodWords.length - 1))] + 'Go to level ' + (level + 1);
 
 		level++;    
 		setTimeout(() => {
@@ -187,14 +187,14 @@ const goNextLevel = () => {
 
 const playerLose = () => {
 	// As a player, I would like to see a message showing that I lose.
-	targetLevel.innerText = 'YOU MISSED IT. YOU LOSE! Click RESTART to play again!';
+	status.innerText = 'Oops! You Missed it! Click RESTART to play again';
 	if (best <= level && level > 1) {
 			best = level - 1;
 	}
 }
 
 const playerWin = () => {
-	targetLevel.innerText = 'YOU WIN! Click RESTART to play again!'
+	status.innerText = 'YOU WIN! Click RESTART to play again'
 	if (best <= level) {
 			best = level;
 	}
@@ -202,12 +202,12 @@ const playerWin = () => {
 
 restart.onclick = () => {
 	colorblock.style.display = 'none';
-	targetLevel.innerText = 'Loading...';
+	status.innerText = 'Loading...';
 	setTimeout(() => {
-		targetLevel.innerText = '';
+		status.innerText = '';
 		colorblock.style.display = 'flex';
 		restartGame();
-}, 600 + (sequence.length * 2000));
+	}, 600 + (sequence.length * 2000));
 }
 
 // During the game player can change difficulty, level records will reset
@@ -241,10 +241,6 @@ changeDifficulity.onclick = () => {
 const restartGame = () => {
 	colorblock.classList.add('unclickable');
 
-	// const restart = prompt('Do you want to restart the game?', 'yes/no');
-	// if (restart === 'yes') {
-		
-	// }
 	for (item of colors) {
 		item.style.backgroundColor = getRGB();
 	}
@@ -258,9 +254,8 @@ const restartGame = () => {
 // Issues:
 // [Violation] 'click' handler
 // Error with Permissions-Policy header: Unrecognized feature: 'interest-cohort'. -> only in github
-// change alerts to something
-// restart the game === 'no' go gameover
-// gameover function
+
+// gameover button
 
 // Bonus:
 
